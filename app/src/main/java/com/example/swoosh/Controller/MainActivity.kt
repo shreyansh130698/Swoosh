d23c4c696d5d6fb1d2785f28d3990a41d94e0ec2
Shreyansh <shreyanshsrvstv.bst98@gmail.com>
2020-06-21 12:04:08 +0530

    Resource for Button. 1. res>drawable>new drawable>swoosh_button.

---
+++ app/src/main/java/com/example/swoosh/Controller/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.swoosh.Controller
+
+import android.content.Intent
+import android.os.Bundle
+import com.example.swoosh.R
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        getStartedBtn.setOnClickListener{
+            val leagueIntent = Intent(this, LeagueActivity::class.java)
+            startActivity(leagueIntent)
+        }
+    }
+}
